f34fb24635e85d029556b19c4f1bd7013caf36d9
McBochi <McBochi@users.noreply.github.com>
2016-04-28 14:41:23 +0200

    socketService now handles incoming messages
    Frontend now shows debug messages received on the websocket, controllers
    can add handlers for message types

--- gui.charger/src/main/resources/webapp/app/components/content/debugController.js
+++ gui.charger/src/main/resources/webapp/app/components/content/debugController.js
@@ -1,4 +1,17 @@
 angular.module('ChargerUI')
   .controller('debugController', function ($scope, $log, socketService) {
-    $scope.socket = socketService.socket;
+    $scope.debugmsgs = [];
+
+    var debugHandler = function(json) {
+      var currentdate = new Date();
+      var datetime = ('0' + currentdate.getHours()).slice(-2) + ":"
+                      + ('0' + currentdate.getMinutes()).slice(-2) + ":"
+                      + ('0' + currentdate.getSeconds()).slice(-2); // TODO format string
+
+      var message = {time : datetime, message : json.message};
+      $scope.debugmsgs.unshift(message);
+      $scope.$apply();
+    };
+
+    socketService.addHandler('debug', debugHandler);
   });
--- gui.charger/src/main/resources/webapp/app/shared/websocket/socketService.js
+++ gui.charger/src/main/resources/webapp/app/shared/websocket/socketService.js
@@ -1,6 +1,7 @@
 angular.module("ChargerUI")
   .factory("socketService", function ($log) {
     var socket = new WebSocket("ws://localhost:8081"); // TODO Config -> provider()
+    var callback = {}
 
     socket.onopen = function() {
       $log.info("Socket successfully opened.");
@@ -10,13 +11,22 @@ angular.module("ChargerUI")
       $log.error("Socket error: " + error);
     };
 
-    socket.onmessage = function(msg) {
-      $log.debug("Socket received: " + msg.data);
+    socket.onmessage = function(evt) {
+      $log.debug("Socket received: " + evt.data);
+
+      var msg = JSON.parse(evt.data);
+      if (msg.hasOwnProperty('type') && msg.type in callback) {
+        callback[msg.type](msg.content);
+      } else {
+        $log.error("No callback registered for received message!");
+      }
     }
 
-    return {
-      socket: function () {
-        return socket;
+    sockServ = {
+      addHandler: function (msgType, handler) {
+        callback[msgType] = handler;
       }
     };
+
+    return sockServ;
   });
