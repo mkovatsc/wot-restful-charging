fc65355d4d17a59666f17cfd0553fba002ba520c
McBochi <McBochi@users.noreply.github.com>
2016-04-28 16:10:33 +0200

    Added statusController

---
+++ gui.charger/src/main/resources/webapp/app/components/content/statusController.js
@@ -0,0 +1,11 @@
+angular.module('ChargerUI')
+  .controller('statusController', function ($scope, $log, socketService) {
+
+    var statusHandler = function(json) {
+      $scope.se = json.se;
+      $scope.ev = json.ev;
+      $scope.$apply();
+    };
+
+    socketService.addHandler('status', statusHandler);
+  });
