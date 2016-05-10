cdacd89ecfa3780b3f33e4d43d2ca9284d93be9f
McBochi <McBochi@users.noreply.github.com>
2016-05-10 15:52:46 +0200

    Callback based on MessageType

--- gui.charger/src/main/java/de/uni_passau/fim/bochenek/ma/gui/charger/handler/MessageHandler.java
+++ gui.charger/src/main/java/de/uni_passau/fim/bochenek/ma/gui/charger/handler/MessageHandler.java
@@ -5,6 +5,8 @@ import java.util.logging.Logger;
 
 import de.uni_passau.fim.bochenek.ma.gui.charger.SocketHandler;
 import de.uni_passau.fim.bochenek.ma.lib.charger.handler.IHandler;
+import de.uni_passau.fim.bochenek.ma.lib.charger.messages.Message;
+import de.uni_passau.fim.bochenek.ma.lib.charger.messages.Message.MessageType;
 
 public class MessageHandler implements IHandler {
 
@@ -18,6 +20,11 @@ public class MessageHandler implements IHandler {
 	public void callback(String msg) {
 		SocketHandler.getInstance().pushToListeners(msg);
 		logger.log(Level.INFO, "{0} received callback: {1}", new Object[]{this.getClass().getSimpleName(), msg});
+	}
+
+	public void callback(MessageType type, Message msg) {
+		SocketHandler.getInstance().pushToListeners(type, msg);
+		logger.log(Level.INFO, "{0} received callback of type {1}", new Object[]{this.getClass().getSimpleName(), type});
 	}
 
 }
--- lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/handler/IHandler.java
+++ lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/handler/IHandler.java
@@ -1,6 +1,10 @@
 package de.uni_passau.fim.bochenek.ma.lib.charger.handler;
 
+import de.uni_passau.fim.bochenek.ma.lib.charger.messages.Message;
+import de.uni_passau.fim.bochenek.ma.lib.charger.messages.Message.MessageType;
+
 /**
+ * TODO
  * 
  * @author Martin Bochenek
  *
@@ -9,5 +13,6 @@ public interface IHandler {
 
 	public void callback();
 	public void callback(String msg);
+	public void callback(MessageType type, Message msg);
 
 }
