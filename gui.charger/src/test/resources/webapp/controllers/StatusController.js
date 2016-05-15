1453f605e011c23bda788d7946ffc0b337dbb5ab
McBochi <McBochi@users.noreply.github.com>
2016-05-15 23:21:15 +0200

    Reformatting tests for ChargerUI

--- gui.charger/src/test/resources/webapp/controllers/StatusController.js
+++ gui.charger/src/test/resources/webapp/controllers/StatusController.js
@@ -1,39 +1,39 @@
 describe('Testing the StatusController', function() {
 
-    beforeEach(module('ChargerUI'));
+  beforeEach(module('ChargerUI'));
 
-    var $controller;
+  var $controller;
 
-    beforeEach(inject(function(_$controller_){
-      $controller = _$controller_;
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  describe('$scope', function() {
+    var $scope, controller;
+
+    beforeEach(function() {
+      $scope = {};
+      controller = $controller('statusController', { $scope: $scope });
+    });
+
+    it('updates the status of the supply equipment', inject(function(statusService) {
+      var se_data = { a : "b", c : 12.3, d : false};
+
+      statusService.se.set(se_data);
+      expect($scope.se.data.a).toEqual("b");
+      expect($scope.se.data.c).toEqual(12.3);
+      expect($scope.se.data.d).toEqual(false);
     }));
 
-    describe('$scope', function() {
-      var $scope, controller;
+    it('updates the status of the electric vehicle', inject(function(statusService) {
+      var ev_data = { aa : "bb", cc : 32.1, dd : true};
 
-      beforeEach(function() {
-        $scope = {};
-        controller = $controller('statusController', { $scope: $scope });
-      });
+      statusService.ev.set(ev_data);
+      expect($scope.ev.data.aa).toEqual("bb");
+      expect($scope.ev.data.cc).toEqual(32.1);
+      expect($scope.ev.data.dd).toEqual(true);
+    }));
 
-      it('updates the status of the supply equipment', inject(function(statusService) {
-        var se_data = { a : "b", c : 12.3, d : false};
-
-        statusService.se.set(se_data);
-        expect($scope.se.data.a).toEqual("b");
-        expect($scope.se.data.c).toEqual(12.3);
-        expect($scope.se.data.d).toEqual(false);
-      }));
-
-      it('updates the status of the electric vehicle', inject(function(statusService) {
-        var ev_data = { aa : "bb", cc : 32.1, dd : true};
-
-        statusService.ev.set(ev_data);
-        expect($scope.ev.data.aa).toEqual("bb");
-        expect($scope.ev.data.cc).toEqual(32.1);
-        expect($scope.ev.data.dd).toEqual(true);
-      }));
-
-    });
+  });
 
 });
