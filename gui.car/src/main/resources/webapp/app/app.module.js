50d056e846df253a760ef4593d346a338516f7fb
McBochi <McBochi@users.noreply.github.com>
2016-05-15 23:57:14 +0200

    Setup basic state machine for the car

--- gui.car/src/main/resources/webapp/app/app.module.js
+++ gui.car/src/main/resources/webapp/app/app.module.js
@@ -1,1 +1,58 @@
-var app = angular.module('CarUI', ['ngMaterial', 'ngRoute', 'ngCookies']);
+var app = angular.module('CarUI', ['ngMaterial', 'ngRoute', 'FSM']);
+
+app.config(['stateMachineProvider', function(stateMachineProvider) {
+  
+  stateMachineProvider.config({
+    init : {
+      transitions : {
+        pluggedIn : 'chargeParameterDiscovery'
+      }
+    },
+    chargeParameterDiscovery : {
+      transitions : {
+        chargeParameterDiscovery : 'chargeParameterDiscovery',
+        chargeParameterDiscoveryDone : 'cableCheck'
+      }
+    },
+    cableCheck : {
+      transitions : {
+        cableCheck : 'cableCheck',
+        cableCheckDone : 'preCharge'
+      }
+    },
+    preCharge : {
+      transitions : {
+        preCharge : 'preCharge',
+        preChargeDone : 'powerDelivery'
+      }
+    },
+    powerDelivery : {
+      transitions : {
+        chargeParameterDiscovery : 'chargeParameterDiscovery',
+        powerDeliveryDone : 'currentDemand',
+        powerDeliveryStopW : 'weldingDetection',
+        powerDeliveryStopS : 'sessionStop'
+      }
+    },
+    currentDemand : {
+      transitions : {
+        currentDemand : 'currentDemand',
+        currentDemandStop : 'powerDelivery'
+      }
+    },
+    weldingDetection : {
+      transitions : {
+        weldingDetection : 'weldingDetection',
+        weldingDetectionDone : 'sessionStop'
+      }
+    },
+    sessionStop : {
+
+    }
+  });
+
+}]);
+
+app.run(['stateMachine', function(stateMachine) {
+    stateMachine.initialize();
+}]);
