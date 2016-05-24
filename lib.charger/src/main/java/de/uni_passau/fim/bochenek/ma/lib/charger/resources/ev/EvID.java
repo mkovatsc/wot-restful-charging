db4ba3cf3d849640d21c2d1bf9156d4f491a7382
McBochi <McBochi@users.noreply.github.com>
2016-05-24 15:19:53 +0200

    First try: dynamic resources

---
+++ lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/ev/EvID.java
@@ -0,0 +1,12 @@
+package de.uni_passau.fim.bochenek.ma.lib.charger.resources.ev;
+
+import org.eclipse.californium.core.CoapResource;
+
+public class EvID extends CoapResource {
+
+	public EvID(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/ev/EvRoot.java
+++ lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/ev/EvRoot.java
@@ -1,12 +1,30 @@
 package de.uni_passau.fim.bochenek.ma.lib.charger.resources.ev;
 
+import java.util.UUID;
+
 import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.eclipse.californium.core.server.resources.CoapExchange;
 
 public class EvRoot extends CoapResource {
 
 	public EvRoot(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handlePOST(CoapExchange exchange) {
+		UUID uuid = UUID.randomUUID();
+		EvID ev = new EvID(uuid.toString());
+		ev.add(new EvChargingComplete("chargingComplete"));
+		ev.add(new EvMaxValues("maxValues"));
+		ev.add(new EvReadyToCharge("readyToCharge"));
+		ev.add(new EvSoc("stateOfCharge"));
+		ev.add(new EvTargetValues("targetValues"));
+		this.add(ev);
+
+		exchange.respond(ResponseCode.CREATED);
 	}
 
 }
