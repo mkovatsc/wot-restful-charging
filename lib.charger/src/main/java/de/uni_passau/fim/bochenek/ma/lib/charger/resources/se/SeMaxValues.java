931d43a5d7c1e9328cd47d7bec6d2de643b2112f
McBochi <McBochi@users.noreply.github.com>
2016-05-24 21:44:30 +0200

    Turned SE resources into HalResources

--- lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/se/SeMaxValues.java
+++ lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/se/SeMaxValues.java
@@ -1,12 +1,56 @@
 package de.uni_passau.fim.bochenek.ma.lib.charger.resources.se;
 
-import org.eclipse.californium.core.CoapResource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class SeMaxValues extends CoapResource {
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import black.door.hate.HalRepresentation;
+import black.door.hate.HalRepresentation.HalRepresentationBuilder;
+import black.door.hate.HalResource;
+
+public class SeMaxValues extends CoapResource implements HalResource {
+
+	private double current;
+	private double voltage;
 
 	public SeMaxValues(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handleGET(CoapExchange exchange) {
+		try {
+			exchange.respond(ResponseCode.CONTENT, this.asEmbedded().serialize(), MediaTypeRegistry.APPLICATION_JSON);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public URI location() {
+		try {
+			return new URI(this.getURI());
+		} catch (URISyntaxException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public HalRepresentationBuilder representationBuilder() {
+		HalRepresentationBuilder hal = HalRepresentation.builder();
+		hal.addLink("self", this);
+		hal.addProperty("current", current);
+		hal.addProperty("voltage", voltage);
+
+		return hal;
 	}
 
 }
--- lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/se/SeRoot.java
+++ lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/se/SeRoot.java
@@ -1,12 +1,56 @@
 package de.uni_passau.fim.bochenek.ma.lib.charger.resources.se;
 
-import org.eclipse.californium.core.CoapResource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class SeRoot extends CoapResource {
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+import org.eclipse.californium.core.server.resources.Resource;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import black.door.hate.HalRepresentation;
+import black.door.hate.HalResource;
+import black.door.hate.HalRepresentation.HalRepresentationBuilder;
+
+public class SeRoot extends CoapResource implements HalResource {
 
 	public SeRoot(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handleGET(CoapExchange exchange) {
+		try {
+			exchange.respond(ResponseCode.CONTENT, this.asEmbedded().serialize(), MediaTypeRegistry.APPLICATION_JSON);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public URI location() {
+		try {
+			return new URI(this.getURI());
+		} catch (URISyntaxException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public HalRepresentationBuilder representationBuilder() {
+		HalRepresentationBuilder hal = HalRepresentation.builder();
+		hal.addLink("self", this);
+
+		for (Resource res : this.getChildren()) {
+			hal.addLink(res.getName(), (HalResource) res);
+		}
+
+		return hal;
 	}
 
 }
