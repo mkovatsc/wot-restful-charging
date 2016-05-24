0a9d04dfa41cdf763618e0f1d2a9b4ceb8200e2e
McBochi <McBochi@users.noreply.github.com>
2016-05-24 15:27:05 +0200

    Allow EVs to be deleted from CoAP server

--- lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/ev/EvID.java
+++ lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/ev/EvID.java
@@ -1,12 +1,20 @@
 package de.uni_passau.fim.bochenek.ma.lib.charger.resources.ev;
 
 import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.eclipse.californium.core.server.resources.CoapExchange;
 
 public class EvID extends CoapResource {
 
 	public EvID(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handleDELETE(CoapExchange exchange) {
+		this.delete();
+		exchange.respond(ResponseCode.DELETED);
 	}
 
 }
