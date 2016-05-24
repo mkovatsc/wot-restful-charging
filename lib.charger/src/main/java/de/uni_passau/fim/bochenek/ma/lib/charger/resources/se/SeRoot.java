97ea2aeb4a01037bb2adaa71849fb45a2427f2f6
McBochi <McBochi@users.noreply.github.com>
2016-05-24 15:02:08 +0200

    New resource: SeRoot

---
+++ lib.charger/src/main/java/de/uni_passau/fim/bochenek/ma/lib/charger/resources/se/SeRoot.java
@@ -0,0 +1,12 @@
+package de.uni_passau.fim.bochenek.ma.lib.charger.resources.se;
+
+import org.eclipse.californium.core.CoapResource;
+
+public class SeRoot extends CoapResource {
+
+	public SeRoot(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
