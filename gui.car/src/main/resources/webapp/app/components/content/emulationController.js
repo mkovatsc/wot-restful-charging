67ef6b665e6daa40efe55e9307fc8596ef9f7905
McBochi <McBochi@users.noreply.github.com>
2016-06-09 17:58:44 +0200

    Pushing car to rootScope

--- gui.car/src/main/resources/webapp/app/components/content/emulationController.js
+++ gui.car/src/main/resources/webapp/app/components/content/emulationController.js
@@ -1,12 +1,10 @@
 app.controller('emulationController', function ($log, $rootScope, $scope, carService, emulationService) {
 
-  // Define some $scope variables
-  $scope.currentState = '-';
-
   // Create a new car
   var car = new carService({
     socketaddr: 'ws://localhost:8091'
   });
+  $rootScope.car = car; // Assign it to $rootScope for global state access
 
   // Create a new emulator for the car
   var emulator = new emulationService({
