e62eb0ad2ec38da12f26ce35424ef4d988baf6e1
McBochi <McBochi@users.noreply.github.com>
2016-07-24 16:25:41 +0200

    Add debug message handler to car emulator

--- gui.charger/src/main/resources/webapp/app/components/content/emulationController.js
+++ gui.charger/src/main/resources/webapp/app/components/content/emulationController.js
@@ -1,9 +1,23 @@
-app.controller('emulationController', function ($log, $rootScope, $scope, chargerService) {
+app.controller('emulationController', function ($log, $rootScope, $scope, chargerService, debugService) {
 
   // Create a new charger
   var charger = new chargerService({
     socketaddr: 'ws://localhost:8081'
   });
   $rootScope.charger = charger;
+
+  // Create and add debug message handler to charger
+  var debugHandler = function(json) {
+    var currentdate = new Date();
+    var datetime = ('0' + currentdate.getHours()).slice(-2) + ":"
+                    + ('0' + currentdate.getMinutes()).slice(-2) + ":"
+                    + ('0' + currentdate.getSeconds()).slice(-2); // TODO format string
+
+    debugService.pushMsg(datetime, json.message);
+    $rootScope.$apply();
+  };
+
+  charger.config.socket.addHandler('DEBUG', debugHandler);
+  $rootScope.debugmsgs = debugService.messages;
 
 });
