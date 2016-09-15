b6f054b8e17f0f028653a7680680cbb52c788840
McBochi <McBochi@users.noreply.github.com>
2016-09-15 19:24:02 +0200

    Allow command line arguments for car and charger
    TODO: Change port in javascript accordingly or allow configuration!

--- gui.car/src/main/java/de/uni_passau/fim/bochenek/ma/gui/car/ServerProvider.java
+++ gui.car/src/main/java/de/uni_passau/fim/bochenek/ma/gui/car/ServerProvider.java
@@ -4,32 +4,84 @@ import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.MissingArgumentException;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.UnrecognizedOptionException;
+
 import de.uni_passau.fim.bochenek.ma.lib.car.handler.SocketHandler;
 import de.uni_passau.fim.bochenek.ma.util.server.GuiServer;
 
 public class ServerProvider {
 
 	// Configuration
-	private static final int appPort = 8090;
-	private static final int socketPort = 8091;
-	private static final URL appUrl = ServerProvider.class.getResource("/webapp");
+	private static final int DEFAULT_PORT_APP = 8090;
+	private static final int DEFAULT_PORT_SOCKET = 8091;
+	private static final URL APP_URL = ServerProvider.class.getResource("/webapp");
 
 	private static Logger logger = Logger.getLogger(ServerProvider.class.getName());
 
 	public static void main(String[] args) {
 
-		// Start GUI server
-		GuiServer server = new GuiServer(appPort, appUrl, socketPort, SocketHandler.getInstance());
-		try {
-			server.start();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// Prepare for command line parsing
+		CommandLineParser parser = new DefaultParser();
 
-		// Debugging information
-		logger.log(Level.INFO, "GuiServer (Application) started on: " + server.getAppPort());
-		logger.log(Level.INFO, "GuiServer (Socket) started on: " + server.getSocketPort());
+		// Create available options
+		Options options = new Options();
+		options.addOption(Option.builder("a")
+				.longOpt("app-port")
+				.desc("The port where the UI will be served.")
+				.hasArg()
+				.type(Integer.class)
+				.build());
+		options.addOption(Option.builder("s")
+				.longOpt("socket-port")
+				.desc("The port where the websocket will be listening.")
+				.hasArg()
+				.type(Integer.class)
+				.build());
+
+		// Try to parse the arguments and print help if something wents wrong
+		try {
+			CommandLine line = parser.parse(options, args);
+
+			int portApp = (line.getParsedOptionValue("a") != null) ? (Integer) line.getParsedOptionValue("a") : DEFAULT_PORT_APP;
+			int portSocket = (line.getParsedOptionValue("s") != null) ? (Integer) line.getParsedOptionValue("s") : DEFAULT_PORT_SOCKET;
+
+			// Start GUI server
+			GuiServer server = new GuiServer(portApp, APP_URL, portSocket, SocketHandler.getInstance());
+			try {
+				server.start();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			// Debugging information
+			logger.log(Level.INFO, "GuiServer (Application) started on: " + server.getAppPort());
+			logger.log(Level.INFO, "GuiServer (Socket) started on: " + server.getSocketPort());
+		} catch (ParseException exp) {
+			HelpFormatter formatter = new HelpFormatter();
+			StringBuilder header = new StringBuilder("Error: ");
+
+			if (exp instanceof MissingOptionException) {
+				header.append("You forgot to specify a required option. ");
+			}
+			if (exp instanceof MissingArgumentException) {
+				header.append("A provided option is missing an argument. ");
+			}
+			if (exp instanceof UnrecognizedOptionException) {
+				header.append("A provided option could not be recognized.");
+			}
+			
+			formatter.printHelp("car", header.toString(), options, "");
+		}
 	}
 
 }
--- gui.charger/src/main/java/de/uni_passau/fim/bochenek/ma/gui/charger/ServerProvider.java
+++ gui.charger/src/main/java/de/uni_passau/fim/bochenek/ma/gui/charger/ServerProvider.java
@@ -8,6 +8,17 @@ import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.MissingArgumentException;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.UnrecognizedOptionException;
+
 import de.uni_passau.fim.bochenek.ma.lib.charger.Charger;
 import de.uni_passau.fim.bochenek.ma.lib.charger.handler.SocketHandler;
 import de.uni_passau.fim.bochenek.ma.lib.charger.messages.Message.MessageType;
@@ -21,38 +32,79 @@ import de.uni_passau.fim.bochenek.ma.util.server.data.ChargerData;
 public class ServerProvider {
 
 	// Configuration
-	private static final int appPort = 8080;
-	private static final int socketPort = 8081;
-	private static final URL appUrl = ServerProvider.class.getResource("/webapp");
+	private static final int DEFAULT_PORT_APP = 8080;
+	private static final int DEFAULT_PORT_SOCKET = 8081;
+	private static final URL APP_URL = ServerProvider.class.getResource("/webapp");
 
 	private static Logger logger = Logger.getLogger(ServerProvider.class.getName());;
 
 	public static void main(String[] args) {
 
-		// Start GUI server
-		GuiServer server = new GuiServer(appPort, appUrl, socketPort, SocketHandler.getInstance());
+		// Prepare for command line parsing
+		CommandLineParser parser = new DefaultParser();
+
+		// Create available options
+		Options options = new Options();
+		options.addOption(Option.builder("a")
+				.longOpt("app-port")
+				.desc("The port where the UI will be served.")
+				.hasArg()
+				.type(Integer.class)
+				.build());
+		options.addOption(Option.builder("s")
+				.longOpt("socket-port")
+				.desc("The port where the websocket will be listening.")
+				.hasArg()
+				.type(Integer.class)
+				.build());
+
+		// Try to parse the arguments and print help if something wents wrong
 		try {
-			server.start();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			CommandLine line = parser.parse(options, args);
+
+			int portApp = (line.getParsedOptionValue("a") != null) ? (Integer) line.getParsedOptionValue("a") : DEFAULT_PORT_APP;
+			int portSocket = (line.getParsedOptionValue("s") != null) ? (Integer) line.getParsedOptionValue("s") : DEFAULT_PORT_SOCKET;
+			
+			// Start GUI server
+			GuiServer server = new GuiServer(portApp, APP_URL, portSocket, SocketHandler.getInstance());
+			try {
+				server.start();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			// Prepare data POJOs for charger and connected cars
+			ChargerData chargerData = new ChargerData();
+			SocketHandler.getInstance().setChargerData(chargerData); // TODO Part of an ugly hack...
+
+			// Setup and start charger
+			Charger charger = new Charger(chargerData);
+			charger.start();
+
+			// Debugging information
+			logger.log(Level.INFO, "GuiServer (Application) started on: " + server.getAppPort());
+			logger.log(Level.INFO, "GuiServer (Socket) started on: " + server.getSocketPort());
+
+			// Start regular interface update
+			Timer timer = new Timer();
+			timer.schedule(new InterfaceUpdate(chargerData), 0, 1000);
+		} catch (ParseException exp) {
+			HelpFormatter formatter = new HelpFormatter();
+			StringBuilder header = new StringBuilder("Error: ");
+
+			if (exp instanceof MissingOptionException) {
+				header.append("You forgot to specify a required option. ");
+			}
+			if (exp instanceof MissingArgumentException) {
+				header.append("A provided option is missing an argument. ");
+			}
+			if (exp instanceof UnrecognizedOptionException) {
+				header.append("A provided option could not be recognized.");
+			}
+
+			formatter.printHelp("car", header.toString(), options, "");
 		}
-
-		// Prepare data POJOs for charger and connected cars
-		ChargerData chargerData = new ChargerData();
-		SocketHandler.getInstance().setChargerData(chargerData); // TODO Part of an ugly hack...
-
-		// Setup and start charger
-		Charger charger = new Charger(chargerData);
-		charger.start();
-
-		// Debugging information
-		logger.log(Level.INFO, "GuiServer (Application) started on: " + server.getAppPort());
-		logger.log(Level.INFO, "GuiServer (Socket) started on: " + server.getSocketPort());
-
-		// Start regular interface update
-		Timer timer = new Timer();
-		timer.schedule(new InterfaceUpdate(chargerData), 0, 1000);
 	}
 
 	static class InterfaceUpdate extends TimerTask {
